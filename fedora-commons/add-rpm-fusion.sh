83170d2225d699853ae97842ef874ccd10431e56
Vincent <vincentvibe3@gmail.com>
2026-09-03 20:27:33 -0400

    fix interactive dnf command being used

--- fedora-commons/add-rpm-fusion.sh
+++ fedora-commons/add-rpm-fusion.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
