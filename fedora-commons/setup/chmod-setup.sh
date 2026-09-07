586250e648b52f689e59d1c695818f296bbdc850
Vincent <vincentvibe3@gmail.com>
2026-09-07 17:03:45 -0400

    adjust old paths

--- fedora-commons/setup/chmod-setup.sh
+++ fedora-commons/setup/chmod-setup.sh
@@ -5,3 +5,9 @@ for file in /usr/share/container-setup/init-scripts/*.sh; do
 		chmod +x $file
 	fi
 done
+
+for file in /usr/share/container-setup/user-init-scripts/*.sh; do
+	if [[ -f $file ]]; then
+		chmod +x $file
+	fi
+done
