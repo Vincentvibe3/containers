4c6b972d229051772d3279fa42d3147090ef4c6f
Vincent <vincentvibe3@gmail.com>
2026-09-07 16:58:45 -0400

    fix paths for systemd unit removal

--- fedora-commons/setup/brew-config.sh
+++ fedora-commons/setup/brew-config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm /usr/lib/system-preset/01-homebrew.preset
-rm /usr/lib/system/brew-setup.service
-rm /usr/lib/system/brew-update.service
-rm /usr/lib/system/brew-update.timer
-rm /usr/lib/system/brew-upgrade.service
-rm /usr/lib/system/brew-upgrade.timer
+rm /usr/lib/systemd/system-preset/01-homebrew.preset
+rm /usr/lib/systemd/system/brew-setup.service
+rm /usr/lib/systemd/system/brew-update.service
+rm /usr/lib/systemd/system/brew-update.timer
+rm /usr/lib/systemd/system/brew-upgrade.service
+rm /usr/lib/systemd/system/brew-upgrade.timer
