ef802f7463f22a834d69e94b62f4d62d75732258
Vincent <vincentvibe3@gmail.com>
2026-09-07 23:40:35 -0400

    remove some unused debug info and add fzf as default tool

--- fedora-commons/setup/init-scripts/install-brew.sh
+++ fedora-commons/setup/init-scripts/install-brew.sh
@@ -10,9 +10,8 @@ if [[ ! -f /etc/.linuxbrew ]]; then
 	echo "Installing homebrew into container"
 	mkdir -p /tmp/homebrew
 	mkdir -p /home/linuxbrew
-	tar --zstd -xvf /usr/share/homebrew.tar.zst -C /tmp/homebrew
+	tar --zstd -xf /usr/share/homebrew.tar.zst -C /tmp/homebrew
 	cp -R -n /tmp/homebrew/home/linuxbrew/.linuxbrew /home/linuxbrew
-	ls -la /home/linuxbrew/.linuxbrew
 	chown -R $USER_UID:$USER_GID /home/linuxbrew
 	rm -rf /tmp/homebrew
 	touch /etc/.linuxbrew
