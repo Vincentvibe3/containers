9d121f1ad965a9fbd51db5d6ae74fbada56c612c
Vincent <vincentvibe3@gmail.com>
2026-09-09 19:43:00 -0400

    remove redundant paths and add git-credential-manager by default

--- fedora-commons/setup/user-init-scripts/brew-preinstall.sh
+++ fedora-commons/setup/user-init-scripts/brew-preinstall.sh
@@ -2,7 +2,7 @@
 
 # Add brew to path for installation
 if [[ -d /home/linuxbrew/.linuxbrew ]]; then
-	PATH=$PATH:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
+	PATH=$PATH:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin
 	# Install all default brewfiles
 	for file in /usr/share/container-setup/brewfiles/*.brewfile; do
 		if [[ -f $file ]]; then
